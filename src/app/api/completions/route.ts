383f5ed41ae4b6b55a45f054ff502900bb3ab707
Артём <pexfex@mail.ru>
2025-09-08 23:53:37 +0400

    feat: added 0.1.0 version

---
+++ src/app/api/completions/route.ts
@@ -0,0 +1,37 @@
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const prompt = body.prompt;
+    const result = await fetch("https://api.cerebras.ai/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.CEREBRAS_API_KEY}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        model: "llama3.1-8b",
+        messages: [
+          {
+            role: "system",
+            content: "Отвечай на все вопросы максимум в 5 словах",
+          },
+          {
+            role: "user",
+            content: prompt,
+          },
+        ],
+        logprobs: true,
+        top_logprobs: 3,
+      }),
+    });
+
+    console.log(result);
+    const response = await result.json();
+    return NextResponse.json({ response });
+  } catch (err) {
+    console.error("Cerebras error:", err);
+    return NextResponse.json({ error: "Failed to generate" }, { status: 500 });
+  }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,220 @@
-import Image from "next/image";
-
+"use client";
+import { useEffect, useState, useRef } from "react";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import { Skeleton } from "@/components/ui/skeleton";
+import { shuffle } from "./helpers";
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [assistantAnswer, setAssistantAnswer] = useState<string[]>([]);
+  const [assistantLogProbs, setAssistantLogProbs] = useState();
+  const [currentToken, setCurrentToken] = useState<number>(-1);
+  const [currentLogTokens, setLogTokens] = useState<string[]>([]);
+  const [userPredictedAnswer, setUserPredictedAnswer] = useState<string[]>([]);
+  const [AssistantLogProbslength, setAssistantLogProbslength] = useState(0);
+  const [answerTime, setAnswerTime] = useState(0);
+  const [prompt, setPrompt] = useState("");
+  const [isGameStarted, setIsGameStarted] = useState(false);
+  const [predictedPercent, setPredictedPercent] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isRoundFinished, setIsRoundFinished] = useState(false);
+  const intervalRef = useRef<ReturnType<typeof setInterval> | null>(null);
+  const [messages, setMessages] = useState<{ role: string; content: string }[]>(
+    []
+  );
+  const startGame = async () => {
+    setIsRoundFinished(false);
+    setAnswerTime(0)
+    if (userPredictedAnswer.length !== 0) {
+      setMessages((prev) => [
+        ...prev,
+        {
+          role: "assistant",
+          content: userPredictedAnswer.join(""),
+        },
+      ]);
+    }
+    setIsLoading(true);
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+      intervalRef.current = null;
+    }
+    const response = await fetch("/api/completions", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        prompt: prompt,
+      }),
+    });
+    const data = await response.json();
+    data.response.choices[0].logprobs.content.forEach((logProb) => {
+      if (logProb.token !== "<|eot_id|>") {
+        setAssistantAnswer((prev) => [...prev, logProb.token]);
+      }
+    });
+    setAssistantLogProbs(data.response.choices[0].logprobs.content);
+    setAssistantLogProbslength(
+      data.response.choices[0].logprobs.content.length
+    );
+    setUserPredictedAnswer([]);
+    setCurrentToken(0);
+    setIsGameStarted(true);
+    intervalRef.current = setInterval(() => {
+      setAnswerTime((prev) => prev + 1);
+    }, 1000);
+    setIsLoading(false);
+  };
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const currentVariant = () => {
+    if (!assistantLogProbs) return;
+    const variants: string[] = [];
+    for (const logProb of assistantLogProbs[currentToken].top_logprobs) {
+      if (logProb.token !== "<|eot_id|>") {
+        variants.push(logProb.token);
+      }
+    }
+    const mainToken = assistantLogProbs[currentToken].token;
+    if (mainToken !== "<|eot_id|>" && !variants.includes(mainToken)) {
+      variants.push(mainToken);
+    }
+    setLogTokens(shuffle(variants));
+  };
+
+  const selectToken = async (token: string) => {
+    setUserPredictedAnswer((prev) => [...prev, token]);
+    setCurrentToken((prev) => prev + 1);
+  };
+  useEffect(() => {
+    if (currentToken + 1 == AssistantLogProbslength && isGameStarted) {
+      FinishGame();
+    } else {
+      currentVariant();
+    }
+  }, [currentToken]);
+
+  const FinishGame = () => {
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+      intervalRef.current = null;
+    }
+    setPredictedPercent(checkCorrectTokens());
+    setMessages((prev) => [
+      ...prev,
+      {
+        role: "user",
+        content: prompt,
+      },
+    ]);
+    setPrompt("");
+    setIsRoundFinished(true);
+    setIsGameStarted(false);
+  };
+  useEffect(() => {
+    return () => {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+        intervalRef.current = null;
+      }
+    };
+  }, []);
+  const checkCorrectTokens = () => {
+    let correct = 0;
+
+    for (let i = 0; i < AssistantLogProbslength; i++) {
+      if (userPredictedAnswer[i] == assistantAnswer[i]) {
+        correct++;
+      }
+    }
+
+    return Math.round((correct / AssistantLogProbslength) * 100);
+  };
+  return (
+    <main className="w-screen h-full flex flex-col items-center">
+      <div className="h-full flex flex-col items-center relative max-w-[640px]">
+        <div className="pt-14 flex flex-col gap-12 pb-[200px]">
+          {messages?.map((message) => (
+            <div
+              className="flex flex-col items-end w-[640px]"
+              key={Math.random()}
+            >
+              {message.role == "user" && message.content !== "" ? (
+                <div className="bg-[#323232d9] max-w-[448px] text-white px-4 py-[12px] h-fit rounded-[18px]">
+                  <p className=" text-white break-words">{message.content}</p>
+                </div>
+              ) : (
+                <div className="w-full">
+                  <p className=" text-white">{message.content}</p>
+                </div>
+              )}
+            </div>
+          ))}
+          <div className=" flex flex-col gap-10 items-end w-[640px]">
+            {(isLoading || isGameStarted) && (
+              <div className="bg-[#323232d9] max-w-[448px] text-white px-4 py-[12px] h-fit rounded-[18px]">
+                <p className=" text-white break-words">{prompt}</p>
+              </div>
+            )}
+            <div className=" w-full">
+              {currentToken + 1 == AssistantLogProbslength &&
+                isRoundFinished == true && (
+                  <div className="flex gap-4 mb-2">
+                    <p className="text-white">
+                      Думал на протяжении {answerTime}s
+                    </p>
+                    <p className="text-white">
+                      Токены верно угаданы на {predictedPercent}%
+                    </p>
+                  </div>
+                )}
+              {isLoading && <Skeleton className="h-4 w-[250px]" />}
+              {userPredictedAnswer !== undefined && isLoading !== true && (
+                <p className=" text-white">{userPredictedAnswer.join("")}</p>
+              )}
+              {isGameStarted && (
+                <div className="flex gap-4 items-center flex-col sm:flex-row">
+                  <div className=" flex gap-4">
+                    {currentLogTokens.map((token) => (
+                      <Button
+                        key={Math.random()}
+                        onClick={() => selectToken(token)}
+                        className="bg-[#ffffff1a] rounded-[38px]"
+                      >
+                        {token}
+                      </Button>
+                    ))}
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+        <div className=" bg-[#303030] px-5 py-[16px] sticky rounded-[28px] bottom-8  w-[640px] h-fit items-end flex flex-col">
+          <Textarea
+            name="prompt"
+            id="prompt"
+            value={prompt}
+            disabled={isGameStarted || isLoading}
+            onChange={(e) => setPrompt(e.target.value)}
+            placeholder="Задайте вопрос что-бы начать игру..."
+            className=" text-white border-0 placeholder:text-gray-500 w-full hover resize-none overflow-hidden break-words focus-visible:ring-0  focus:outline-none focus:ring-0"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          <Button
+            onClick={startGame}
+            size={"icon"}
+            disabled={isGameStarted || isLoading}
+            className="bg-white rounded-full w-9 h-9 hover:bg-[#ffffff1a]"
+          >
+            <svg
+              width="20"
+              height="20"
+              viewBox="0 0 20 20"
+              fill="black"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M8.99992 16V6.41407L5.70696 9.70704C5.31643 10.0976 4.68342 10.0976 4.29289 9.70704C3.90237 9.31652 3.90237 8.6835 4.29289 8.29298L9.29289 3.29298L9.36907 3.22462C9.76184 2.90427 10.3408 2.92686 10.707 3.29298L15.707 8.29298L15.7753 8.36915C16.0957 8.76192 16.0731 9.34092 15.707 9.70704C15.3408 10.0732 14.7618 10.0958 14.3691 9.7754L14.2929 9.70704L10.9999 6.41407V16C10.9999 16.5523 10.5522 17 9.99992 17C9.44764 17 8.99992 16.5523 8.99992 16Z"></path>
+            </svg>
+          </Button>
+        </div>
+      </div>
+    </main>
   );
 }
